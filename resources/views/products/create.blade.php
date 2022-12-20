fc8ff0bd20cf1a1d19f85877b115c8577b64d490
Gotenks3 <riku.murakami333@gmail.com>
2022-12-21 05:04:24 +0900

    product取り込み＆Formrequest

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,58 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            商品一覧
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div>
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />  
+                        <form action="{{ route('product.store')}}" method="post" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="mb-5">
+                                <image-preview />
+                                <input type="file" name="image2" accept=“image/png,image/jpeg,image/jpg” >
+                                <input type="file" name="image3" accept=“image/png,image/jpeg,image/jpg” >
+                                <input type="file" name="image4" accept=“image/png,image/jpeg,image/jpg” >
+                            </div>
+
+                            <hr>
+                            {{-- 名前、商品情報 --}}
+                            <div>
+                                商品名:<input type="text" name="name">
+                                商品情報:<input type="text" name="content">
+                                料金:<input type="number" name="price">
+                            </div>
+                            <button class="flex ml-auto text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品を登録</button>
+
+                            [これを後々実装] <br>
+                            カテゴリー: 必須> <br>
+
+                            商品の状態: 必須> <br>
+
+                            {{-- 販売 --}}
+                            <select name="is_selling" id="is_selling">
+                                @foreach($sell as $key => $value)
+                                <option value="{{$key}}">{{$value}}</option>
+                                @endforeach
+                            </select>
+
+                            {{-- 商品の状態 --}}
+                            <select name="state" id="state">
+                                @foreach($status as $key => $value)
+                                <option value="{{$key}}">{{$value}}</option>
+                                @endforeach
+                            </select>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
