38cf6a0e55b5ca443df97291b014a51702a27318
Gotenks3 <riku.murakami333@gmail.com>
2022-12-21 09:05:54 +0900

    Vue ImagePreview

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,8 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('image-preview1-component', require('./components/ImagePreview1Component.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/ImagePreview1Component.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <input 
+            type="file" 
+            ref="preview" 
+            required 
+            name="image1" 
+            accept=“image/png,image/jpeg,image/jpg”
+            @change="uploadFile">
+        <div v-if="url">
+            <img :src="url">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            url: ""
+        }
+    },
+    methods: {
+        uploadFile() {
+            const file = this.$refs.preview.files[0];
+            this.url = URL.createObjectURL(file)
+        }
+    }
+}
+</script>
+
+<style>
+    img{
+        width: 120px;
+    }
+</style>
