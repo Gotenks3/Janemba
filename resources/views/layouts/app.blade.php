9296ad51340404be4e428853db5726cc29a6e6ad
Gotenks3 <riku.murakami333@gmail.com>
2022-12-23 08:02:45 +0900

    mypage,レイアウト作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,15 +66,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    {{-- Profile --}}
-                                    <a class="dropdown-item" href="{{ route('profile.index') }}">
-                                        Profile
+  
+                                    {{-- mypage --}}
+                                    <a class="dropdown-item" href="{{ route('mypage') }}">
+                                        Mypage
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 
 
 /*
@@ -18,6 +19,11 @@ use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// マイページ
+Route::prefix('mypage')->group(function(){
+    Route::get('/', [UserController::class, 'mypage'])->name('mypage');
 });
 
 // プロフィール
