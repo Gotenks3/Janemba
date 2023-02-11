e0591d9eec1c5c55351fc3db73ee36b52d1101de
Gotenks3 <riku.murakami333@gmail.com>
2023-02-11 20:33:31 +0900

    画面修正

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -118,7 +118,7 @@
             </div>
         </div>
         <div class="flex justify-center mt-16">
-            <button type="button" onclick="location.href='{{ route('product.index')}}'" class="bg-gray-200 border-0 py-2 px-8 mr-7 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+            <button type="button" onclick="location.href='{{ route('home')}}'" class="bg-gray-200 border-0 py-2 px-8 mr-7 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
             <button type="button" onclick="location.href='{{ route('product.edit', ['product' => $product->id])}}'" class="bg-blue-300 border-0 py-2 px-8 ml-7 focus:outline-none hover:bg-blue-400 rounded text-lg">編集する</button>
         </div>
     </div>
