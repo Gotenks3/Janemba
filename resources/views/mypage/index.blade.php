2a7289a7a62e99a4078232bc80467db8d048d445
Gotenks3 <riku.murakami333@gmail.com>
2023-02-18 23:01:08 +0900

    デプロイ　テスt

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -9,7 +9,7 @@
             <h1 class="mb-20">マイページ一覧</h1>
             </h2>
             <div class="text-blue-100 mb-6">
-                <button type="button" onclick="location.href='{{ route('mypage.profile.index')}}'" class="border w-2/6 p-3 bg-gray-800 rounded hover:bg-gray-500">プロフィール</button>
+                <button type="button" onclick="location.href='{{ route('mypage.profile.index')}}'" class="border w-2/6 p-3 bg-blue-800 rounded hover:bg-gray-500">プロフィール</button>
             </div>
             <div class="text-blue-100 mb-6">
                 <button type="button" onclick="location.href='{{ route('mypage.product.index')}}'" class="border w-2/6 p-3 bg-gray-800 rounded hover:bg-gray-500">商品一覧</button>
