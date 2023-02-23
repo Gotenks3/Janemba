166d203b01e9d3c2706b88650c733b11b6634931
Gotenks3 <riku.murakami333@gmail.com>
2023-02-24 03:31:23 +0900

    決済処理

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -35,6 +35,10 @@
                     <div class="my-2">
                       小計: {{ number_format($totalPrice)}}<span class="text-sm text-gray-700">円(税込)</span>
                     </div>
+                    <div>
+                      <button onclick="location.href='{{ route('product.cart.checkout')}}'" class="flex px-6 py-2 ml-auto text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">購入する
+                      </button>
+                    </div>
                   @else
                     カートに商品が入っていません。
                   @endif
